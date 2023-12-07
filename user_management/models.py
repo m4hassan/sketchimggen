b9becf9dba9a2a33d76fabe553af9f9a8f88c115
m4hassan <96425227+m4hassan@users.noreply.github.com>
2023-12-07 18:10:31 +0500

    Update models.py
    beginning to fix merge conflicts from here. this is a check point commit. adding userprofile front end and image generations gallery

--- user_management/models.py
+++ user_management/models.py
@@ -5,11 +5,11 @@ from PIL import Image
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    first_name = models.CharField(max_length=60, blank=True)
-    last_name = models.CharField(max_length=60, blank=True)
+    # first_name = models.CharField(max_length=60, blank=True)
+    # last_name = models.CharField(max_length=60, blank=True)
     email = models.EmailField(max_length=254, blank=False, null=False, unique=True)
     phone_number = models.CharField(max_length=15, blank=True, null=False)
-    avatar = models.ImageField(upload_to='avatars', default="download.png", blank=True, null=True)
+    avatar = models.ImageField(upload_to='avatars', default="avatars/download.png", blank=True, null=True)
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
@@ -25,3 +25,10 @@ class UserProfile(models.Model):
         #     img.thumbnail(output_size)
         #     # overwrite the larger image
         #     img.save(self.avatar.path)
+
+class GenerationDetails(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    input_img = models.URLField()
+    response_json = models.JSONField()
+    output_img = models.URLField()
+    generated_at = models.DateTimeField(auto_now_add=True)
